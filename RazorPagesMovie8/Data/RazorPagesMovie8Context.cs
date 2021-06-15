c2c6426c774e1e4dc474f019abb7d446ce895cd8
Scot <bscot.hagen@gmail.com>
2021-06-15 10:27:46 -0700

    Add project files.

---
+++ RazorPagesMovie8/Data/RazorPagesMovie8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie8.Models;
+
+namespace RazorPagesMovie8.Data
+{
+    public class RazorPagesMovie8Context : DbContext
+    {
+        public RazorPagesMovie8Context (DbContextOptions<RazorPagesMovie8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie8.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ RazorPagesMovie8/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesMovie8.Data;
+using RazorPagesMovie8.Models;
+
+namespace RazorPagesMovie8.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPagesMovie8.Data.RazorPagesMovie8Context _context;
+
+        public CreateModel(RazorPagesMovie8.Data.RazorPagesMovie8Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
