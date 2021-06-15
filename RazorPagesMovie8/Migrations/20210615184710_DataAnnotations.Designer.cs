398e358fe84747c4b19b73bec8e1d30c0b57575f
Scot <bscot.hagen@gmail.com>
2021-06-15 11:48:39 -0700

    Added Validations to model fields

--- RazorPagesMovie8/Migrations/20210615184710_DataAnnotations.Designer.cs
+++ RazorPagesMovie8/Migrations/20210615184710_DataAnnotations.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMovie8.Data;
 
 namespace RazorPagesMovie8.Migrations
 {
     [DbContext(typeof(RazorPagesMovie8Context))]
-    partial class RazorPagesMovie8ContextModelSnapshot : ModelSnapshot
+    [Migration("20210615184710_DataAnnotations")]
+    partial class DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
