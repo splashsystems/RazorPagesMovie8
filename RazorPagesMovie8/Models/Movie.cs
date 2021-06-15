4db60f1dbb5f0ae745751d07d356a4ecad5b87ee
Scot <bscot.hagen@gmail.com>
2021-06-15 11:40:06 -0700

    Added Search and new field, Rating

--- RazorPagesMovie8/Models/Movie.cs
+++ RazorPagesMovie8/Models/Movie.cs
@@ -17,5 +17,6 @@ namespace RazorPagesMovie8.Models
         public string Genre { get; set; }
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
 }
